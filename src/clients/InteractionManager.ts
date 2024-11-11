4c8d35c8d902d076c8f8671af980bb2f1f8a0b35
Rollphes <70063670+Rollphes@users.noreply.github.com>
2024-11-11 16:50:53 +0900

    [add]interactionManager.execute

--- src/clients/InteractionManager.ts
+++ src/clients/InteractionManager.ts
@@ -2,8 +2,6 @@ import {
   ApplicationCommandType,
   Client,
   ComponentType,
-  DiscordAPIError,
-  HTTPError,
   Interaction,
   Snowflake,
 } from 'discord.js'
@@ -14,22 +12,14 @@ export class InteractionManager {
   private cache: Map<string, InteractionExecuteData> = new Map()
   private commands: Map<string, ApplicationCommandData> = new Map()
 
-  constructor(private client: Client) {
-    client.on('ready', () => {
-      void (async (): Promise<void> => {
-        if (!client.application?.owner) await client.application?.fetch()
-      })()
-    })
+  constructor(private client: Client) {}
 
-    client.on('interactionCreate', (interaction) => {
-      void (async (): Promise<void> => {
-        await Promise.all([
-          this.messageComponentRun(interaction),
-          this.autoCompleteRun(interaction),
-          this.commandRun(interaction),
-        ])
-      })()
-    })
+  public async execute(interaction: Interaction): Promise<void> {
+    await Promise.all([
+      this.messageComponentExecute(interaction),
+      this.autoCompleteExecute(interaction),
+      this.commandExecute(interaction),
+    ])
   }
 
   public set(customId: string, data: InteractionExecuteData): void {
@@ -66,74 +56,67 @@ export class InteractionManager {
     else await this.client.application.commands.set(data)
   }
 
-  private async messageComponentRun(interaction: Interaction): Promise<void> {
+  private async messageComponentExecute(
+    interaction: Interaction,
+  ): Promise<void> {
     if (!interaction.isModalSubmit() && !interaction.isMessageComponent())
       return
     const interactionExecute = this.cache.get(interaction.customId)
     if (!interactionExecute || !interactionExecute.execute) return
 
-    try {
-      switch (interactionExecute.type) {
-        case ComponentType.Button:
-          if (interaction.isButton())
-            await interactionExecute.execute(interaction)
-          break
-        case ComponentType.StringSelect:
-          if (interaction.isStringSelectMenu())
-            await interactionExecute.execute(interaction)
-          break
-        case ComponentType.UserSelect:
-          if (interaction.isUserSelectMenu())
-            await interactionExecute.execute(interaction)
-          break
-        case ComponentType.RoleSelect:
-          if (interaction.isRoleSelectMenu())
-            await interactionExecute.execute(interaction)
-          break
-        case ComponentType.ChannelSelect:
-          if (interaction.isChannelSelectMenu())
-            await interactionExecute.execute(interaction)
-          break
-        case ComponentType.MentionableSelect:
-          if (interaction.isMentionableSelectMenu())
-            await interactionExecute.execute(interaction)
-          break
-        case ComponentType.TextInput:
-          if (interaction.isModalSubmit())
-            await interactionExecute.execute(interaction)
-          break
-      }
-    } catch (err) {
-      this.interactionErrorCatch(err, interaction)
+    switch (interactionExecute.type) {
+      case ComponentType.Button:
+        if (interaction.isButton())
+          await interactionExecute.execute(interaction)
+        break
+      case ComponentType.StringSelect:
+        if (interaction.isStringSelectMenu())
+          await interactionExecute.execute(interaction)
+        break
+      case ComponentType.UserSelect:
+        if (interaction.isUserSelectMenu())
+          await interactionExecute.execute(interaction)
+        break
+      case ComponentType.RoleSelect:
+        if (interaction.isRoleSelectMenu())
+          await interactionExecute.execute(interaction)
+        break
+      case ComponentType.ChannelSelect:
+        if (interaction.isChannelSelectMenu())
+          await interactionExecute.execute(interaction)
+        break
+      case ComponentType.MentionableSelect:
+        if (interaction.isMentionableSelectMenu())
+          await interactionExecute.execute(interaction)
+        break
+      case ComponentType.TextInput:
+        if (interaction.isModalSubmit())
+          await interactionExecute.execute(interaction)
+        break
     }
   }
 
-  private async commandRun(interaction: Interaction): Promise<void> {
+  private async commandExecute(interaction: Interaction): Promise<void> {
     if (!interaction.isCommand()) return
     const command = this.commands.get(interaction.commandName)
     if (!command || !command.execute) return
 
-    try {
-      switch (command.type) {
-        case ApplicationCommandType.User:
-          if (interaction.isUserContextMenuCommand())
-            await command.execute(interaction)
-          break
-        case ApplicationCommandType.Message:
-          if (interaction.isMessageContextMenuCommand())
-            await command.execute(interaction)
-          break
-        default:
-          if (interaction.isChatInputCommand())
-            await command.execute(interaction)
-          break
-      }
-    } catch (err) {
-      this.interactionErrorCatch(err, interaction)
+    switch (command.type) {
+      case ApplicationCommandType.User:
+        if (interaction.isUserContextMenuCommand())
+          await command.execute(interaction)
+        break
+      case ApplicationCommandType.Message:
+        if (interaction.isMessageContextMenuCommand())
+          await command.execute(interaction)
+        break
+      default:
+        if (interaction.isChatInputCommand()) await command.execute(interaction)
+        break
     }
   }
 
-  private async autoCompleteRun(interaction: Interaction): Promise<void> {
+  private async autoCompleteExecute(interaction: Interaction): Promise<void> {
     if (!interaction.isAutocomplete()) return
     const command = this.commands.get(interaction.commandName)
     if (
@@ -144,30 +127,6 @@ export class InteractionManager {
       return
     if (!command || !command.autoComplete) return
 
-    try {
-      await command.autoComplete(interaction)
-    } catch (err) {
-      this.interactionErrorCatch(err, interaction)
-    }
-  }
-
-  private interactionErrorCatch(err: unknown, interaction: Interaction): void {
-    if (
-      !(err instanceof HTTPError && err.message === 'Service Unavailable') &&
-      !(err instanceof DiscordAPIError && err.message === 'Unknown interaction')
-    )
-      throw err
-
-    if (interaction.isCommand())
-      console.error(`ErrorInteractionCommandName => ${interaction.commandName}`)
-
-    if (interaction.isModalSubmit() || interaction.isMessageComponent())
-      console.error(`ErrorInteractionCustomId => ${interaction.customId}`)
-    if (interaction.isAutocomplete()) {
-      console.error(
-        `ErrorInteractionAutoCompleteCommandName => ${interaction.commandName}`,
-      )
-    }
-    console.error(err)
+    await command.autoComplete(interaction)
   }
 }
